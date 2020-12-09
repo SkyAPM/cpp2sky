aab3ba6517179516341daa7c4f50784416ed5946
Shikugawa <Shikugawa@gmail.com>
2020-12-09 19:48:05 +0900

    improve example

--- example/sample.cc
+++ example/sample.cc
@@ -12,4 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-int main(void) { return 0; }
+#include <chrono>
+#include <string>
+
+#include "cpp2sky/propagation.h"
+#include "cpp2sky/segment_context.h"
+#include "cpp2sky/tracer.h"
+#include "httplib.h"
+
+using namespace cpp2sky;
+
+static const std::string service_name = "";
+static const std::string instance_name = "node_0";
+static const std::string address = "0.0.0.0:11800";
+
+SegmentConfig seg_config(service_name, instance_name);
+TracerConfig tracer_config(address);
+
+uint64_t now() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch())
+      .count();
+}
+
+int main() {
+  httplib::Server svr;
+  // 1. Create tracer object to send span data to OAP.
+  auto tracer = createInsecureGrpcTracer(tracer_config);
+
+  svr.Get("/ping", [&](const httplib::Request& req, httplib::Response& res) {
+    // 2. Create segment context
+    auto current_segment = createSegmentContext(seg_config);
+
+    // 3. Initialize span data to track root workload on current service.
+    auto current_span = current_segment->createCurrentSegmentRootSpan();
+
+    // 4. Set info
+    current_span->setOperationName("/ping");
+    current_span->setStartTime(now());
+    current_span->setEndTime(now());
+
+    // 5. Send span data
+    tracer->sendSegment(std::move(current_segment));
+  });
+
+  svr.listen("0.0.0.0", 8081);
+  return 0;
+}
