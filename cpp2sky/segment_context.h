4875a52431d0f368d48ba067adc6ee12f7aef6fa
Shikugawa <Shikugawa@gmail.com>
2020-12-20 19:20:40 +0900

    fix factory

--- cpp2sky/segment_context.h
+++ cpp2sky/segment_context.h
@@ -215,7 +215,7 @@ using SegmentContextPtr = std::shared_ptr<SegmentContext>;
 
 class SegmentContextFactory {
  public:
-  ~SegmentContextFactory() = default;
+  virtual ~SegmentContextFactory() = default;
 
   /**
    * Create segment context that doesn't have propagated info.
